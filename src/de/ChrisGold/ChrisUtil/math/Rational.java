9a40063814b825707a3e2df3116419462dcc5dcf
CTMGame <ctmgame@38.de>
2016-11-07 21:58:14 +0100

    Refactor to make immutable

--- src/de/ChrisGold/ChrisUtil/math/Rational.java
+++ src/de/ChrisGold/ChrisUtil/math/Rational.java
@@ -2,94 +2,52 @@ package de.ChrisGold.ChrisUtil.math;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.math.MathContext;
+import java.math.RoundingMode;
 
 /**
  * @author Christian Goldapp
  */
-public class Rational{
+public class Rational {
 
-    public static final Rational ONE = new Rational(1);
-    public static final Rational HALF = new Rational(0.5);
-    public static final Rational MINUS_ONE = new Rational(-1);
-    public static final Rational PI = new Rational(Math.PI);
-    public static final Rational E = new Rational(Math.E);
+    public static final Rational ONE = Rational.valueOf(1);
+    public static final Rational HALF = Rational.valueOf(0.5);
+    public static final Rational MINUS_ONE = Rational.valueOf(-1);
+    public static final Rational PI = Rational.valueOf(Math.PI);
+    public static final Rational E = Rational.valueOf(Math.E);
 
-    private BigInteger numerator = BigInteger.ONE;
-    private BigInteger denominator = BigInteger.ONE;
+    private final BigInteger numerator;
+    private final BigInteger denominator;
 
     /**
      * Constructs a new {@code Rational}.
+     *
      * @param val A {@code BigInteger} that represents the value.
      */
-    public Rational(BigInteger val){
+    private Rational(BigInteger val) {
         this.numerator = val;
-        reduce();
+        this.denominator = BigInteger.ONE;
     }
 
     /**
      * Constructs a new {@code Rational}.
-     * @param numerator A {@code BigInteger} that represents the numerator.
+     *
+     * @param numerator   A {@code BigInteger} that represents the numerator.
      * @param denominator A {@code BigInteger} that represents the denominator.
      */
-    public Rational(BigInteger numerator, BigInteger denominator){
+    private Rational(BigInteger numerator, BigInteger denominator) {
         this.numerator = numerator;
         this.denominator = denominator;
-        reduce();
     }
 
-    /**
-     * Constructs a new {@code Rational}.
-     * @param val A {@code BigDecimal} that represents the value.
-     */
-    public Rational(BigDecimal val){
-        int sign = val.signum();
-        int scale = val.scale();
-        //Work with absolute values from now on
-        val = val.abs();
-
-        if (scale == 0) {
-            this.numerator = val.unscaledValue();
-            this.denominator = BigInteger.ONE;
-        } else if (scale < 0) {
-            this.numerator = val.unscaledValue().multiply(BigInteger.TEN.pow(-1 * scale));
-            this.denominator = BigInteger.ONE;
-        } else if (scale > 0) {
-            this.numerator = val.unscaledValue();
-            this.denominator = BigInteger.TEN.pow(scale);
-        } else assert false;
-
-        //Restore the sign.
-        if (sign < 0) numerator = numerator.multiply(BigInteger.valueOf(-1));
-
-        reduce();
+    @Override
+    public String toString() {
+        return toString(10);
     }
 
-    /**
-     * Constructs a new {@code Rational}.
-     * @param val A {@code double} that represents the value.
-     */
-    public Rational(double val){
-        this(BigDecimal.valueOf(val));
-    }
-
-    /**
-     * Constructs a new {@code Rational}.
-     * @param val A {@code long} that represents the value.
-     */
-    public Rational(long val){
-        this(BigInteger.valueOf(val));
-    }
-
-    /**
-     * Reduces the {@code Rational} by setting numerator and denominator to the smallest possible pair of whole numbers
-     * that can represent the value.
-     */
-    private void reduce(){
-        if(denominator.equals(BigInteger.ZERO)) throw new ArithmeticException("Denominator is zero!");
-        BigInteger gcd = numerator.gcd(denominator);
-        numerator = numerator.divide(gcd);
-        denominator = denominator.divide(gcd);
+    public BigDecimal toBigDecimal() {
+        BigDecimal num = new BigDecimal(numerator);
+        BigDecimal den = new BigDecimal(denominator);
+        return num.divide(den, RoundingMode.HALF_DOWN);
     }
 
     public int intValue(){
@@ -108,19 +66,8 @@ public class Rational{
         return numerator.divide(denominator);
     }
 
-    public BigDecimal toBigDecimal(){
-        BigDecimal decNum = new BigDecimal(numerator);
-        BigDecimal decDen = new BigDecimal(denominator);
-        return decNum.divide(decDen, MathContext.DECIMAL128);
-    }
-
-    @Override
-    public String toString() {
-        return String.format("(%d / %d)", numerator, denominator);
-    }
-
     public String toString(int radix) {
-        return String.format("(%s / %s)", numerator.toString(radix), denominator.toString(radix));
+        return String.format("%s / %s", numerator.toString(radix), denominator.toString(radix));
     }
 
     public String toDecimalString(){
@@ -130,15 +77,15 @@ public class Rational{
     public Rational add(Rational val){
         BigInteger num = this.numerator.multiply(val.denominator).add(val.numerator.multiply(this.denominator));
         BigInteger den = this.denominator.multiply(val.denominator);
-        return new Rational(num, den);
+        return Rational.valueOf(num, den);
     }
 
     public Rational add(long val){
-        return this.add(new Rational(val));
+        return this.add(Rational.valueOf(val));
     }
 
     public Rational add(double val){
-        return this.add(new Rational(val));
+        return this.add(Rational.valueOf(val));
     }
 
     public Rational subtract(Rational val){
@@ -147,42 +94,97 @@ public class Rational{
     }
 
     public Rational subtract(long val){
-        return this.subtract(new Rational(val));
+        return this.subtract(Rational.valueOf(val));
     }
 
     public Rational subtract(double val){
-        return this.subtract(new Rational(val));
+        return this.subtract(Rational.valueOf(val));
     }
 
     public Rational multiply(Rational val){
-        return new Rational(this.numerator.multiply(val.numerator), this.denominator.multiply(val.denominator));
+        return Rational.valueOf(this.numerator.multiply(val.numerator), this.denominator.multiply(val.denominator));
     }
 
     public Rational multiply(long val){
-        return new Rational(this.numerator.multiply(BigInteger.valueOf(val)), this.denominator);
+        return Rational.valueOf(this.numerator.multiply(BigInteger.valueOf(val)), this.denominator);
     }
 
     public Rational multiply(double val){
-        return this.multiply(new Rational(val));
+        return this.multiply(Rational.valueOf(val));
     }
 
     public Rational divide(Rational val){
-        return new Rational(this.numerator.multiply(val.denominator), this.denominator.multiply(val.numerator));
+        return Rational.valueOf(this.numerator.multiply(val.denominator), this.denominator.multiply(val.numerator));
     }
 
     public Rational divide(long val){
-        return divide(new Rational(val));
+        return divide(Rational.valueOf(val));
     }
 
     public Rational divide(double val){
-        return divide(new Rational(val));
+        return divide(Rational.valueOf(val));
     }
 
     public Rational abs(){
-        return new Rational(numerator.abs(), denominator.abs());
+        return Rational.valueOf(numerator.abs(), denominator.abs());
     }
 
     public Rational pow(int val){
-        return new Rational(numerator.pow(val), denominator.pow(val));
+        return Rational.valueOf(numerator.pow(val), denominator.pow(val));
+    }
+
+    public static Rational valueOf(BigDecimal val) {
+        BigInteger numerator = BigInteger.ONE;
+        BigInteger denominator = BigInteger.ONE;
+        int sign = val.signum();
+        int scale = val.scale();
+        //Work with absolute values from now on
+        val = val.abs();
+
+        if (scale == 0) {
+            numerator = val.unscaledValue();
+            denominator = BigInteger.ONE;
+        } else if (scale < 0) {
+            numerator = val.unscaledValue().multiply(BigInteger.TEN.pow(-1 * scale));
+            denominator = BigInteger.ONE;
+        } else if (scale > 0) {
+            numerator = val.unscaledValue();
+            denominator = BigInteger.TEN.pow(scale);
+        }
+
+        //Restore the sign.
+        if (sign < 0) {
+            numerator = numerator.multiply(BigInteger.valueOf(-1));
+        }
+
+        //Reduce the result
+        return valueOf(numerator, denominator);
+    }
+
+    public static Rational valueOf(BigInteger numerator, BigInteger denominator) {
+        BigInteger gcd = numerator.gcd(denominator);
+        numerator = numerator.divide(gcd);
+        denominator = denominator.divide(gcd);
+        return new Rational(numerator, denominator);
+    }
+
+    public static Rational valueOf(double val) {
+        return valueOf(BigDecimal.valueOf(val));
+    }
+
+    public static Rational valueOf(String s) {
+        if (s.contains("/")) {
+            return valueOf(Double.parseDouble(s));
+        }
+        String[] parts = s.split("/");
+        BigInteger numerator = new BigInteger(parts[0]);
+        BigInteger denominator = new BigInteger(parts[1]);
+        return valueOf(numerator, denominator);
+    }
+
+    public static void main(String[] args) {
+        Rational r = Rational.valueOf(Math.PI);
+        r = r.pow(1000);
+        System.out.println(r);
     }
 }
